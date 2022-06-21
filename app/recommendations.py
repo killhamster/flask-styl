d63f2eca52ea7c61bb64fbf44095318c6559fa03
KH <killhamster@gmail.com>
2022-06-21 13:51:06 -0700

    Some songs have no recommendations in the Spotify API. Added code to avoid this leading to errors

--- app/recommendations.py
+++ app/recommendations.py
@@ -13,37 +13,50 @@ def get_recommendations(search):
     #Get ID of the track. Need a list for the recommendation lookup
     id_list = [result['tracks']['items'][0]['id']]
 
-    return sp.recommendations(seed_tracks = id_list, limit = 10)
+    recs = sp.recommendations(seed_tracks = id_list, limit = 10)
+
+    if recs['tracks'] == False:
+        return None
+    else:
+        return recs
 
 def get_covers(recommendations):
-
     #Init
     sp = spotipy.Spotify(client_credentials_manager = SpotifyClientCredentials())
 
     id_list = [track['id'] for track in recommendations['tracks']]
 
-    tracks = sp.tracks(id_list)
-
-    cover_links = [image['album']['images'][2]['url'] for image in tracks['tracks']]
-
-    return cover_links
+    if not id_list:
+        return None
+    else:
+        tracks = sp.tracks(id_list)
+        cover_links = [image['album']['images'][2]['url'] for image in tracks['tracks']]
+        return cover_links
 
 def display(recommendations, covers):
-
     html_display = ''
-    for idx, track in enumerate(recommendations['tracks']):
+    if len(recommendations['tracks']) != 0:
+        for idx, track in enumerate(recommendations['tracks']):
 
+            html_display += f'''
+                <div class = 'row recommendation'>
+                    <div class = 'col-md-2'>
+                        <img src = "{covers[idx]}">
+                    </div>
+                    <div class = 'col-md-6 info'>
+                        <p>{track['name']} by {track['artists'][0]['name']}</p>
+                    </div>
+                    <div class = 'col-md-4'>
+                        <a href = "{track['external_urls']['spotify']}" target = '_blank'><button class = 'button'>Play It</button></a>
+                    </div>
+                </div>
+            '''            
+    else:
         html_display += f'''
-            <div class = 'row recommendation'>
-                <div class = 'col-md-2'>
-                    <img src = "{covers[idx]}">
+                <div class = 'row recommendation'>
+                    <div class = 'col-md-8 info'>
+                        <p>Bummer! Spotify doesn't know enough about this track or artist!</p>
+                    </div>
                 </div>
-                <div class = 'col-md-6 info'>
-                    <p>{track['name']} by {track['artists'][0]['name']}</p>
-                </div>
-                <div class = 'col-md-4'>
-                    <a href = "{track['external_urls']['spotify']}" target = '_blank'><button class = 'button'>Play It</button></a>
-                </div>
-            </div>
-        '''
+            '''            
     return html_display
--- app/routes.py
+++ app/routes.py
@@ -16,13 +16,13 @@ def index():
     form = SearchForm()
 
     if form.validate_on_submit():
-
+        
         recommendations = get_recommendations(form.search.data)
         covers = get_covers(recommendations)
         html_code = display(recommendations, covers)
 
 
-        return render_template('index.html', title = 'STYL - Recommendations based on that one song you love', \
-            form = form, html_code = html_code)
+        return render_template('index.html', title = 'STYL - Recommendations based on that one Song That You Love', \
+                form = form, html_code = html_code)
 
-    return render_template('index.html', title = 'STYL - Recommendations based on that one song you love', form = form)
+    return render_template('index.html', title = 'STYL - Recommendations based on that one Song That You Love', form = form)
